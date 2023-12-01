06d29984d500c294dfc406c4cb079143f5a2cd46
MatVzn <matteovoleite@gmail.com>
2023-11-30 22:04:29 -0300

    testing js idea

--- js/script.js
+++ js/script.js
@@ -8,5 +8,6 @@ var navItens = document.querySelectorAll('.NavItem')
 navItens.forEach(function(navIten) {
   navIten.addEventListener('mouseenter', function() {
     navIten.classList.remove("NavSelect")
+    navIten.classList.add("NavSelect")
   });
 });
