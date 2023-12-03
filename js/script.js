2ac9cfc4f00f7e49b09caa0485873d013b8fdda2
MatVzn <matteovoleite@gmail.com>
2023-12-02 21:36:45 -0300

    semantic nav

--- js/script.js
+++ js/script.js
@@ -8,6 +8,5 @@ var navItens = document.querySelectorAll('.NavItem')
 navItens.forEach(function(navIten) {
   navIten.addEventListener('mouseenter', function() {
     navIten.classList.remove("NavSelect")
-    navIten.classList.add("NavSelect")
   });
 });
