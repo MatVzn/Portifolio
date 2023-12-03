4939b53ed217b241086489e577d53905bbfa1000
MatVzn <matteovoleite@gmail.com>
2023-12-03 13:08:37 -0300

    nav menu completed

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
-function NavHover(event) {
-  var navItem = event.target
-  var navItems = document.querySelectorAll('.NavItem')
-  navItems.forEach(function(item) {
-    if (item === navItem) {
-      // Se o mouse está sobre este item, remove a classe NavHover
-      item.classList.remove('NavHover');
-    } else {
-      // Se o mouse não está sobre este item, adiciona a classe NavHover
-      item.classList.add('NavHover')
-    }
-  })
-}
-
-function NavHoverReset() {
+document.addEventListener('DOMContentLoaded', function() {
   var navItems = document.querySelectorAll('.NavItem')
 
+  function navHoverHandler(event) {
+    var targetNavItem = event.target
+    navItems.forEach(function(item) {
+      if (item === targetNavItem) {
+        // Se o mouse está sobre este item, remove a classe NavHover
+        item.classList.remove('NavHover')
+      } else {
+        // Se o mouse não está sobre este item, adiciona a classe NavHover
+        item.classList.add('NavHover')
+      }
+    })
+  }
+
+  function navHoverResetHandler() {
+    navItems.forEach(function(item) {
+      // Remove a classe NavHover para todos os itens
+      item.classList.remove('NavHover')
+    })
+  }
+
+  // Adiciona ouvintes de evento aos itens de navegação
   navItems.forEach(function(item) {
-    // Remove a classe NavHover para todos os itens
-    item.classList.remove('NavHover')
+    item.addEventListener('mouseover', navHoverHandler)
+    item.addEventListener('mouseout', navHoverResetHandler)
   })
-}
+})
