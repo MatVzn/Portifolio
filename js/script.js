45b8b870a9f05ba4683df03077b75afd4bd934d9
MatVzn <matteovoleite@gmail.com>
2023-12-02 22:08:54 -0300

    test new nav

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
+function NavHover(event) {
+  var navItem = event.target
+  var navItems = document.querySelectorAll('.NavItem')
+  navItems.forEach(function(item) {
+    if (item === navItem) {
+      // Se o mouse está sobre este item, remove a classe NavHover
+      item.classList.remove('NavHover');
+    } else {
+      // Se o mouse não está sobre este item, adiciona a classe NavHover
+      item.classList.add('NavHover')
+    }
+  })
+}
 
+function NavHoverReset() {
+  var navItems = document.querySelectorAll('.NavItem')
 
-
-
-
-var navItens = document.querySelectorAll('.NavItem')
-
-navItens.forEach(function(navIten) {
-  navIten.addEventListener('mouseenter', function() {
-    navIten.classList.remove("NavSelect")
-  });
-});
+  navItems.forEach(function(item) {
+    // Remove a classe NavHover para todos os itens
+    item.classList.remove('NavHover')
+  })
+}
