f2fef0b6a15aa5b4afbd8ffc78601898a5a624b2
MatVzn <matteovoleite@gmail.com>
2024-10-13 19:51:02 -0300

    update logos

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"usWYIBM4F/BgqJE8Tis0AcqUrbF4pIeN2jgcut2wAjw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"5dqxs+oVxhubDhcwM9+bAwnjHJ3BhPkjcaHIdpNmWzI=\"\n}"
